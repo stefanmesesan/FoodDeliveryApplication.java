48c9c3e2178e775c9db5ff21919b47849d391f4c
stefanmesesan <stefan.mesesan12@gmail.com>
2023-05-15 01:31:04 +0300

    jwt implementation

---
+++ demo/src/main/java/com/example/licenta/controller/AuthenticationController.java
@@ -0,0 +1,29 @@
+package com.example.licenta.controller;
+
+import com.example.licenta.security.AuthenticationRequest;
+import com.example.licenta.security.AuthenticationResponse;
+import com.example.licenta.security.RegisterRequest;
+import com.example.licenta.service.impl.AuthenticationServiceImpl;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/auth")
+@RequiredArgsConstructor
+public class AuthenticationController {
+
+    private final AuthenticationServiceImpl authenticationService;
+    @PostMapping("/register")
+    public ResponseEntity<AuthenticationResponse> register(@RequestBody RegisterRequest request) {
+        return ResponseEntity.ok(authenticationService.register(request));
+    }
+
+    @PostMapping("/authenticate")
+    public ResponseEntity<AuthenticationResponse> register(@RequestBody AuthenticationRequest request) {
+        return ResponseEntity.ok(authenticationService.authenticate(request));
+    }
+}
---
+++ demo/src/main/java/com/example/licenta/service/impl/AuthenticationServiceImpl.java
@@ -0,0 +1,58 @@
+package com.example.licenta.service.impl;
+
+import com.example.licenta.model.User;
+import com.example.licenta.model.UserRole;
+import com.example.licenta.repository.UserRepository;
+import com.example.licenta.security.AuthenticationRequest;
+import com.example.licenta.security.AuthenticationResponse;
+import com.example.licenta.security.JwtService;
+import com.example.licenta.security.RegisterRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthenticationServiceImpl {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtService jwtService;
+    private final AuthenticationManager authenticationManager;
+    public AuthenticationResponse register(RegisterRequest request) {
+        var user = User.builder()
+                .firstName(request.getFirstName())
+                .lastName(request.getLastName())
+                .email(request.getEmail())
+                .password(passwordEncoder.encode(request.getPassword()))
+                .role(UserRole.CUSTOMER)
+                .build();
+
+        userRepository.save(user);
+
+        var jwtToken = jwtService.generateToken(user);
+
+        return AuthenticationResponse
+                .builder()
+                .token(jwtToken)
+                .build();
+    }
+
+    public AuthenticationResponse authenticate(AuthenticationRequest request) {
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        request.getEmail(),
+                        request.getPassword())
+        );
+        var user = userRepository.findUserByEmail(request.getEmail()).orElseThrow();
+
+        var jwtToken = jwtService.generateToken(user);
+
+        return AuthenticationResponse
+                .builder()
+                .token(jwtToken)
+                .build();
+    }
+}
